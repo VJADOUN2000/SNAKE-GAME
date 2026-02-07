f7f23fcccb1568ad3aaff86ab90067b13366c137
VJADOUN2000 <jadounvikrant@gmail.com>
2026-02-07 23:31:52 +0530

    Create value for snake value and fill style for background

--- script.js
+++ script.js
@@ -7,22 +7,28 @@ const rows = Math.floor(board.clientHeight / blockHeight);
 
 
 const blocks =[];
-
-// for(let i =0; i<rows * cols;i++){
-//     const block = document.createElement('div')
-//     block.classList.add("block")
-//     board.appendChild(block);
-// }
+const snake = [{
+    x:2,y:4
+},{
+    x:2,y:5
+},{
+    x:2,y:6
+}]
 
 for(let row =0;row<rows;row++){
     for (let col =0;col<cols;col++){
         const block = document.createElement('div')
     block.classList.add("block")
     board.appendChild(block);
-    blocks[`${row}'-${col}`] = block
+    block.innerText=`${row}-${col}`
+    blocks[`${row}-${col}`] = block
 
     }
 }
 
-
+function render(){
+    snake.forEach(segment=>{
+        console.log(blocks[`${segment.x}-${segment.y}`])
+    })
+}
 // Testing git commit.
