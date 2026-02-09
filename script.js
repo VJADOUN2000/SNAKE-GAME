6f3d1a6bb9a63e8dc41aafc633de9ca776f28a0e
VJADOUN2000 <jadounvikrant@gmail.com>
2026-02-09 23:14:34 +0530

    Create let for food and use Math.random for appearing in grid

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const blockWidth = 50;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
-
+let intervalId =null;
+let food = {x: Math.floor(Math.random() *rows),y: Math.floor(Math.random() *cols)}
 
 const blocks =[];
 const snake = [
@@ -27,13 +28,9 @@ for(let row =0;row<rows;row++){
 }
 
 function render(){
-    snake.forEach(segment=>{
-        blocks[`${segment.x}-${segment.y}`].classList.add("fill")
-    })
-}
-
-setInterval(()=>{
     let head = null;
+
+    blocks[`${food.x}-${food.y}`].classList.add("food")
 
     if(direction==="left"){
         head ={x:snake[0].x,y:snake[0].y-1}
@@ -44,6 +41,14 @@ setInterval(()=>{
     } else if(direction ==="up"){
         head ={x:snake[0].x-1,y:snake[0].y}
     }
+        if(head.x<0 || head.x>=rows || head.y<0 || head.y >=cols){
+            alert("Game Over!")
+            clearInterval(intervalId)
+        }
+
+        if(head.x==food.x & head.y==food.y){
+            
+        }
 
     snake.forEach(segment=>{
         blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
@@ -52,8 +57,15 @@ setInterval(()=>{
     snake.unshift(head);
     snake.pop();
 
-    render()
-},400)
+    snake.forEach(segment=>{
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill")
+    })
+}
+
+intervalId= setInterval(()=>{
+        render()
+
+},300)
 
 addEventListener("keydown", (event) => {
     if(event.key == "ArrowUp"){
