003a217bea56b67e1c58dc645c48329574b11079
VJADOUN2000 <jadounvikrant@gmail.com>
2026-02-09 22:42:33 +0530

    work on Eventlistencer for arow key

--- script.js
+++ script.js
@@ -32,25 +32,30 @@ function render(){
     })
 }
 
-setInterval(()=>{
-    let head = null;
+// setInterval(()=>{
+//     let head = null;
 
-    if(direction==="left"){
-        head ={x:snake[0].x,y:snake[0].y-1}
-    } else if(direction ==="right"){
-        head ={x:snake[0].x,y:snake[0].y+1}
-    } else if(direction === "down"){
-        head ={x:snake[0].x+1,y:snake[0].y}
-    } else if(direction ==="up"){
-        head ={x:snake[0].x-1,y:snake[0].y}
-    }
+//     if(direction==="left"){
+//         head ={x:snake[0].x,y:snake[0].y-1}
+//     } else if(direction ==="right"){
+//         head ={x:snake[0].x,y:snake[0].y+1}
+//     } else if(direction === "down"){
+//         head ={x:snake[0].x+1,y:snake[0].y}
+//     } else if(direction ==="up"){
+//         head ={x:snake[0].x-1,y:snake[0].y}
+//     }
 
-    snake.forEach(segment=>{
-        blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
-    })
+//     snake.forEach(segment=>{
+//         blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
+//     })
 
-    snake.unshift(head);
-    snake.pop();
+//     snake.unshift(head);
+//     snake.pop();
 
-    render()
-},400)
+//     render()
+// },400)
+
+addEventListener("keydown", (event) => {
+    
+    console.log(event.key);
+});
